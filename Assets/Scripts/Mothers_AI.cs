a7f419f349046df2a185466eb6b01a84d344c58d
KBC12006188\rikut <rikuto-taba-20h@stu.kbc.ac.jp>
2022-01-17 14:39:26 +0900

    往復するだけ

---
+++ Assets/Scripts/Mothers_AI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mothers_AI : MonoBehaviour
+{
+    int counter = 0;
+    float move = 0.05f;
+
+    void Update()
+    {
+        Vector3 p = new Vector3(0, 0, move);
+        transform.Translate(p);
+        counter++;
+
+        //countが100になれば-1を掛けて逆方向に動かす
+        if (counter == 500)
+        {
+            counter = 0;
+            move *= -1;
+        }
+    }
+}
