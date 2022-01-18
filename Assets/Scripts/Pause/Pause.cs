6250a070b65fd0bafbab55b98296cff4b4acaac8
KBC12006094\kbc12006094 <yuuya-kuniyoshi-20h@stu.kbc.ac.jp>
2022-01-18 12:02:31 +0900

    ポーズ画面途中

---
+++ Assets/Scripts/Pause/Pause.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField]
+    GameObject pausePanel;
+    bool pushFlag = false;
+    //bool getcountdown;
+    bool pushscene;
+    //bool GameClearflg;
+
+    private void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        //getcountdown = StartCount.CountDownStart();
+        pushscene = PauseSelect.PushLoadScene();
+        //GameClearflg = GameClear.ClearText();
+
+
+        //if (GameClearflg == false)
+        //{
+        //    if (getcountdown == false)
+        //    {
+                if (pushscene == false)
+                {
+                    if (Input.GetKeyDown(KeyCode.P) || Input.GetButton("Start"))
+                    {
+
+                        if (pushFlag == false)
+                        {
+                            pushFlag = true;
+                            if (Time.timeScale == 1)
+                            {
+                                Time.timeScale = 0;
+                                pausePanel.SetActive(true);
+                            }
+                            else
+                            {
+                                pushFlag = true;
+                                Time.timeScale = 1;
+                                pausePanel.SetActive(false);
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        pushFlag = false;
+                    }
+                    //Debug.Log(pushFlag);
+                }
+        //    }
+        //}
+        //else
+        //{
+        //    GameClearflg = false;
+        //}
+    }
+}
---
+++ Assets/Scripts/Pause/PauseSelect.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseSelect : MonoBehaviour
+{
+    static int MenuNumber = 0;
+    RectTransform rect;
+
+    bool pushFlag = false;
+    bool SEflag = false;
+    private static bool pushScene = false;
+    int StageNumber = 0;
+
+    void Start()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        //SEflag = Seselect.GetSEFlag();
+        //StageNumber = StageSelect.StageNumber();
+
+        if (SEflag == false)
+        {
+            if ((!Input.GetButton("A") && Input.GetAxis("Vertical") == -1) || (!Input.GetButton("A") && Input.GetAxis("Vertical2") == -1))
+            {
+                if (pushFlag == false)
+                {
+                    pushFlag = true;
+                    if (++MenuNumber > 2) MenuNumber = 0;   //ポーズメニューのカーソルが一番下から一番上にくるように
+                }
+            }
+            else if ((!Input.GetButton("A") && Input.GetAxis("Vertical") == 1) || (!Input.GetButton("A") && Input.GetAxis("Vertical2") == 1))
+            {
+                if (pushFlag == false)
+                {
+                    pushFlag = true;
+                    if (--MenuNumber < 0) MenuNumber = 2;    //ポーズメニューのカーソルが一番上から一番下にくるように
+
+                }
+            }
+            else
+            {
+                pushFlag = false;
+            }
+        }
+
+        switch (MenuNumber)
+        {
+            case 0:
+                rect.localPosition = new Vector3(-420, 40, 0);
+                if (Input.GetButton("A") || (Input.GetButton("A") && Input.GetAxis("Vertical") == 1) || (Input.GetButton("A") && Input.GetAxis("Vertical2") == 1) || (Input.GetButton("A") && Input.GetAxis("Vertical") == -1) || (Input.GetButton("A") && Input.GetAxis("Vertical2") == -1))
+                {
+                    pushScene = true;
+                    //StartCoroutine(RetryCoroutine());
+                }
+                //Debug.Log("0");
+                break;
+            case 1:
+                rect.localPosition = new Vector3(-420, -80, 0);
+                if (Input.GetButton("A") || (Input.GetButton("A") && Input.GetAxis("Vertical") == 1) || (Input.GetButton("A") && Input.GetAxis("Vertical2") == 1) || (Input.GetButton("A") && Input.GetAxis("Vertical") == -1) || (Input.GetButton("A") && Input.GetAxis("Vertical2") == -1))
+                {
+                    pushScene = true;
+                    StartCoroutine(TitleCoroutine());
+                }
+                //Debug.Log("1");
+                break;
+            case 2:
+                rect.localPosition = new Vector3(-420, -200, 0);
+                if (Input.GetButton("A") || (Input.GetButton("A") && Input.GetAxis("Vertical") == 1) || (Input.GetButton("A") && Input.GetAxis("Vertical2") == 1) || (Input.GetButton("A") && Input.GetAxis("Vertical") == -1) || (Input.GetButton("A") && Input.GetAxis("Vertical2") == -1))
+                {
+                    pushScene = true;
+                    StartCoroutine(EndCoroutine());
+                }
+                //Debug.Log("2");
+                break;
+
+        }
+    }
+
+
+
+    //private IEnumerator RetryCoroutine()    //StageNumberによってロードシーンを管理
+    //{
+        //yield return new WaitForSecondsRealtime(1.5f);
+
+        //if (StageNumber == 0)
+        //{
+        //    SceneManager.LoadScene("Stage1");
+        //    pushScene = false;
+        //    MenuNumber = 0;
+        //}
+        //else if (StageNumber == 1)
+        //{
+        //    SceneManager.LoadScene("Stage2");
+        //    pushScene = false;
+        //    MenuNumber = 0;
+        //}
+        //else if (StageNumber == 2)
+        //{
+        //    SceneManager.LoadScene("Stage3");
+        //    pushScene = false;
+        //    MenuNumber = 0;
+        //}
+        //else if (StageNumber == 3)
+        //{
+        //    SceneManager.LoadScene("Stage4");
+        //    pushScene = false;
+        //    MenuNumber = 0;
+        //}
+        //Time.timeScale = 1;
+    //}
+
+    private IEnumerator TitleCoroutine()
+    {
+        yield return new WaitForSecondsRealtime(1.5f);
+
+        SceneManager.LoadScene("Title");
+        pushScene = false;
+        Time.timeScale = 1;
+        MenuNumber = 0;
+    }
+
+    private IEnumerator EndCoroutine()
+    {
+        yield return new WaitForSecondsRealtime(1.5f);
+        Application.Quit();
+
+    }
+
+
+    public static bool PushLoadScene()
+    {
+        return pushScene;
+    }
+}
