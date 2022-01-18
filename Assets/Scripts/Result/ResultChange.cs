a5c6454e404a2dd7e9e866d24153bcd6cf38aa79
KBC12006094\kbc12006094 <yuuya-kuniyoshi-20h@stu.kbc.ac.jp>
2022-01-18 10:02:40 +0900

    仮のリザルト作りました

---
+++ Assets/Scripts/Result/ResultChange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResultChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            StartCoroutine(ChangeCoroutine());
+        }
+    }
+
+    private IEnumerator ChangeCoroutine()
+    {
+        yield return new WaitForSecondsRealtime(1.5f);  //1.5秒待った後にシーンをロード
+
+        SceneManager.LoadScene("Title");
+        Time.timeScale = 1;
+    }
+}
