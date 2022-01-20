21ebb525ab48b6fc3673fb2114a483270fb7b831
KBC12006023\kbc12006023 <ryusei-miyazato-20h@stu.kbc.ac.jp>
2022-01-20 11:54:17 +0900

    仮ステージ(とりあえず完成(今後追加予定あるかも))

--- Assets/Scripts/Light/PointLight.cs
+++ Assets/Scripts/Light/PointLight.cs
@@ -7,6 +7,7 @@ public class PointLight : MonoBehaviour
     [SerializeField] float maxIntensity;
     [SerializeField] float blinkSpeed;
 
+    //光の点滅
     Light blinkLight;
 
     int flashAdjustValue = 7;
@@ -20,10 +21,12 @@ public class PointLight : MonoBehaviour
     {
         if(blinkLight.intensity > maxIntensity / flashAdjustValue)
         {
+            //ランダムに点滅させる
             blinkLight.intensity = Mathf.PerlinNoise(Time.time * blinkSpeed, 0) * maxIntensity;
         }
         else
         {
+            //光の強さが変化する
             blinkLight.intensity = Random.Range(0, maxIntensity / 2);
         }
     }
