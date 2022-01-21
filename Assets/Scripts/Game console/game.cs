a7ff34aef1d9eb11b323ec54a84798f3b10474c6
KBC12006023\kbc12006023 <ryusei-miyazato-20h@stu.kbc.ac.jp>
2022-01-21 15:00:09 +0900

    仮ステージ(ゲーム機の電源をつけたり消したりできるようにした)

---
+++ Assets/Scripts/Game console/game.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game : MonoBehaviour
+{
+    //ゲームサウンド
+    private AudioSource audioSource;
+    public AudioClip sound1;
+
+    //ゲームライト
+    public GameObject gamelight;
+
+    void Start()
+    {
+        //AudioComponentを取得
+        audioSource = GetComponent<AudioSource>();
+
+        //ゲームライト（オン）
+        gamelight.SetActive(true);
+    }
+
+    void Update()
+    {
+        //もしBボタンが押されたら
+        if (Input.GetKeyDown(KeyCode.JoystickButton1))
+        {
+            //その時ゲームライトが（オン）なら
+            if (gamelight.activeSelf)
+            {
+                Debug.Log("ボタンが押されている");
+                //ゲームライト（オフ）
+                gamelight.SetActive(false);
+            }
+            else
+            {
+                Debug.Log("もう一度押された");
+                //ゲームサウンドを鳴らす
+                audioSource.PlayOneShot(sound1);
+                //ゲームライト（オン）
+                gamelight.SetActive(true);
+            }
+        }
+    }
+}
