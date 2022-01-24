3b0c434f429a635a8924885687893ea259ca78c0
KBC12006094\kbc12006094 <yuuya-kuniyoshi-20h@stu.kbc.ac.jp>
2022-01-24 15:36:53 +0900

    no message

---
+++ Assets/Scripts/Pause/FirstSelect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirstSelect : MonoBehaviour
+{
+    Button firstSelect;
+
+    void Start()
+    {
+        firstSelect = GameObject.Find("Canvas/Panel/Retry").GetComponent<Button>();
+
+        firstSelect.Select();
+    }
+}
