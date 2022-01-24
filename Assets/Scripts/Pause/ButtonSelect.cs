37f2ebea0992b3ec2e57f7157209e85428591ea9
KBC12006094\kbc12006094 <yuuya-kuniyoshi-20h@stu.kbc.ac.jp>
2022-01-24 14:27:21 +0900

    ポーズ画面途中

--- Assets/Scripts/Pause/ButtonSelect.cs
+++ Assets/Scripts/Pause/ButtonSelect.cs
@@ -2,23 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
 public class ButtonSelect : MonoBehaviour
 {
-    //最初にフォーカスするゲームオブジェクト
-    [SerializeField]
-    private GameObject firstSelect;
+    public Button FirstSelectButton;
 
-    public void ActivateOrNotActivate(bool flag)
+    void Start()
     {
-        for(var i = 0; i < transform.childCount; i++)
-        {
-            transform.GetChild(i).GetComponent<Button>().interactable = flag;
-        }
-        if (flag)
-        {
-            EventSystem.current.SetSelectedGameObject(firstSelect);
-        }
+        FirstSelectButton.Select();
     }
+
 }
---
+++ Assets/Scripts/Pause/Retry.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Retry : MonoBehaviour
+{
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene("SampleScene");
+        //StartCoroutine("RetryCoroutine");
+    }
+
+    //private IEnumerator RetryCoroutine()
+    //{
+    //    yield return new WaitForSecondsRealtime(1.5f);
+
+    //    SceneManager.LoadScene("SampleScene");
+
+    //}
+}
