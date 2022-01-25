55a81d5baf06b2defc6f3374b9f7d5dc56316499
DESKTOP-FH385H9\Noguc <yoshihiro-noguchi-20h@stu.kbc.ac.jp>
2022-01-25 10:22:26 +0900

    アニメーション切り替え

--- Assets/PlayerAnimcontroller.cs
+++ Assets/PlayerAnimcontroller.cs
@@ -1,30 +1,21 @@
 ﻿using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerAnimcontroller : MonoBehaviour
 {
+    Animator m_player;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.m_player = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
     void Update()
     {
-
-        //GetComponentを用いてAnimatorコンポーネントを取り出す.
-        Animator animator = GetComponent<Animator>();
-
-        //あらかじめ設定していたintパラメーター「trans」の値を取り出す.
-        int Hide = animator.GetInteger("Hide");
-
-        //上矢印キーを押した際にパラメータ「trans」の値を増加させる.
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Gamepad.current.bButton.wasPressedThisFrame)
         {
-            Hide++;
+            m_player.SetTrigger("Hidee");
         }
-
-        //下矢印キーを押した際にパラメータ「trans」の値を減少させる.
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            Hide--;
-        }
-
-        //intパラメーターの値を設定する.
-        animator.SetInteger("Hide", Hide);
     }
 }
