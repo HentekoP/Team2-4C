bcc89dc4753027c7d234668c9621b67ebf801317
DESKTOP-FH385H9\Noguc <yoshihiro-noguchi-20h@stu.kbc.ac.jp>
2022-01-25 09:33:21 +0900

    no message

---
+++ Assets/PlayerAnimcontroller.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerAnimcontroller : MonoBehaviour
+{
+
+    void Update()
+    {
+
+        //GetComponentを用いてAnimatorコンポーネントを取り出す.
+        Animator animator = GetComponent<Animator>();
+
+        //あらかじめ設定していたintパラメーター「trans」の値を取り出す.
+        int Hide = animator.GetInteger("Hide");
+
+        //上矢印キーを押した際にパラメータ「trans」の値を増加させる.
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Hide++;
+        }
+
+        //下矢印キーを押した際にパラメータ「trans」の値を減少させる.
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Hide--;
+        }
+
+        //intパラメーターの値を設定する.
+        animator.SetInteger("Hide", Hide);
+    }
+}
