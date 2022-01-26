e9d33c169bd85337b9b0743f18ffa19febc3b1fb
KBC12006023\kbc12006023 <ryusei-miyazato-20h@stu.kbc.ac.jp>
2022-01-26 11:45:30 +0900

    仮ステージ(寝息の音を追加)

--- Assets/PlayerAnimcontroller.cs
+++ Assets/PlayerAnimcontroller.cs
@@ -2,6 +2,11 @@
 using UnityEngine.InputSystem;
 public class PlayerAnimcontroller : MonoBehaviour
 {
+    //ゲームサウンド
+    private AudioSource audioSource;
+
+    bool audioflag = true;
+
     Animator m_player;
 
     bool getSEFlag;
@@ -9,6 +14,9 @@ public class PlayerAnimcontroller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //AudioComponentを取得
+        audioSource = GetComponent<AudioSource>();
+
         this.m_player = GetComponent<Animator>();
     }
 
@@ -22,6 +30,20 @@ public class PlayerAnimcontroller : MonoBehaviour
             if (Gamepad.current.bButton.wasPressedThisFrame)
             {
                 m_player.SetTrigger("Hidee");
+            }
+
+            if (Input.GetKeyDown(KeyCode.JoystickButton1))
+            {
+                if(audioflag == false)
+                {
+                    audioSource.Play();
+                    audioflag = true;
+                }
+                else
+                {
+                    audioSource.Stop();
+                    audioflag = false;
+                }
             }
         }
     }
