68d112687a2ff508dd3193ccee5d60ab204472a2
KBC12006094\kbc12006094 <yuuya-kuniyoshi-20h@stu.kbc.ac.jp>
2022-01-26 12:21:42 +0900

    no message

---
+++ Assets/Scripts/Pause/EnemySound.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySound : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+
+    bool getSEFlag;     //Pauseから受け取る変数用
+    bool pauseflag = true;  //ポーズ状態か管理するためのフラグ
+
+    void Start()
+    {
+        //AudioComponentを取得
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+    void Update()
+    {
+        getSEFlag = Pause.GetSEflag();
+
+        if (getSEFlag == false) //もし getSEFlag が false なら
+        {
+            if (pauseflag == true)
+            {
+                pauseflag = false;
+                if (Time.timeScale == 0)
+                {
+                    audioSource.Stop();
+
+                    //Debug.Log("音がとまる");
+                }
+            }
+        }
+        else　   //もし getSEFlag が true なら
+        {
+            if (pauseflag == false)
+            {
+                pauseflag = true;
+                if (Time.timeScale == 1)
+                {
+                    audioSource.Play();
+                }
+            }
+        }
+    }
+}
