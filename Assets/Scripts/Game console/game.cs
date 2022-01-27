452d72655dd7ae2b6243c2a37e0aa4128e6545dd
KBC12006094\kbc12006094 <yuuya-kuniyoshi-20h@stu.kbc.ac.jp>
2022-01-27 10:38:59 +0900

    スコア加算処理

--- Assets/Scripts/Game console/game.cs
+++ Assets/Scripts/Game console/game.cs
@@ -64,7 +64,8 @@ public class game : MonoBehaviour
                 }
             }
         }
-        //ポーズ画面を閉じると音が鳴りだしてしまう
+
+
 
         if (getSEFlag == true)
         {
@@ -89,16 +90,19 @@ public class game : MonoBehaviour
                     gamelight.SetActive(true);
                     //ゲームサウンドを鳴らす
                     audioSource.Play();
-
-                    while (Light == true)
-                    {
-                        score_num += 1;
-                    }
                 }
 
             }
         }
 
 
+    }
+
+    void FixedUpdate()
+    {
+        if(Light == true)
+        {
+            score_num += 1;
+        }
     }
 }
