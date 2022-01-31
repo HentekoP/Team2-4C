87cfcbfc9c7a6b0e679b27a53000b1a770f715b1
DESKTOP-FH385H9\Noguc <yoshihiro-noguchi-20h@stu.kbc.ac.jp>
2022-01-31 10:08:20 +0900

    敵が歩くように修正

---
+++ Assets/Scripts/enemyAnim.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class enemyAnim : MonoBehaviour
+{
+
+    Animation anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = this.gameObject.GetComponent<Animation>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            anim.Play();
+
+
+
+        }
+
+
+    }
+}
